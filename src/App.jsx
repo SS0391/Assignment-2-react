7de98cb9b6b80138ac7de15f3f7444217eafecbe
Sverre <ss0391web@gmail.com>
2026-09-10 09:46:27 +0200

    setup the fetch and added loading to the cat fact jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import CookieClicker from "./Components/CookieClicker.jsx";
-
+import CatFacts from "./Components/CatFacts.jsx";
 function App() {
   return (
     <div>
       <h1>React assingment 2: useState and useEffect</h1>
-      <CookieClicker />
+      <div>
+        <CookieClicker />
+      </div>
+      <div>
+        <CatFacts />
+      </div>
     </div>
   );
 }
--- src/Components/CatFacts.jsx
+++ src/Components/CatFacts.jsx
@@ -4,4 +4,30 @@ export default function CatFacts() {
   const [facts, setFacts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://catfact.ninja/facts?limit=5")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Can't fetch cat facts");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setFacts(data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Funny and random CatFacts</h2>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
 }
